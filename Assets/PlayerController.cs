790448c9c96eef50f03000c8076813eff82357a4
João Leal <37.joao.leal@gmail.com>
2022-05-13 16:05:57 +0100

    Added run, jump and falling animations

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,26 +1,46 @@
-using System;
 using UnityEngine;
 
 namespace shaman37
 {   
     public class PlayerController : MonoBehaviour
     {
-	    [Header("Movement Settings")]
+        [Header("Movement Settings")]
         [SerializeField] private float     _walkingSpeed = 10f;
 
+        [Space]
         [Header("Jump Settings")]
         [SerializeField] private float     _jumpForce = 10f;
         [SerializeField] private float     _fallMultiplier = 2.5f;
         [SerializeField] private float     _lowJumpMultiplier = 2f;
-        [SerializeField] private float     _groundExtraHeight = 0.05f;
+    
+        [Space]
+        [Header("Wall Slide Settings")]
+        [SerializeField] private float     _slideSpeed = 5f;
+
+        [Space]
+        [Header("Collider Settings")]
         [SerializeField] private LayerMask _mask;
+        [SerializeField] private float     _groundCollisionRadius = 0.05f;
+        [SerializeField] private float     _wallCollisionRadius = 0.2f;
+        [SerializeField] public Vector2    _rightOffset;
+        [SerializeField] public Vector2    _leftOffset;
         
-        // Flags 
-        private bool _jumpRequest = false;
-        private bool _isGrounded = false;
         
-        private Rigidbody2D _rb;
+         
+        private bool wallSlide;
+
+        // Animator bools
+        [SerializeField] private bool _isJumping = false;
+        [SerializeField] private bool _isGrounded = false;
+        [SerializeField] private bool _isOnWall = false;
+        [SerializeField] private bool _isOnRightWall = false;
+        [SerializeField] private bool _isOnLeftWall = false;        
+        
+        // Cached Components
+        private Rigidbody2D       _rb;
         private CapsuleCollider2D _collider;
+        private Animator          _animator;
+        
 
 
         #region [UnityEvents]
@@ -28,36 +48,51 @@ namespace shaman37
         {
             _rb = GetComponent<Rigidbody2D>();
             _collider = GetComponent<CapsuleCollider2D>();
+            _animator = GetComponent<Animator>();
         }
 
         private void Update()
         {
             IsGroundedCheck();
+            IsOnWallsCheck();
 
             float x = Input.GetAxis("Horizontal");
             float y = Input.GetAxis("Vertical");
             Vector2 direction = new Vector2(x, y);
 
             Walk(direction);
-            
+
             if (Input.GetButtonDown("Jump") && _isGrounded)
             {
-                _jumpRequest = true;
+                _isJumping = true;
+                _isGrounded = false;
+
+                _animator.SetTrigger("isJumping");
             }
+
+            if (_rb.velocity.y < 0)
+            {
+                _animator.SetBool("isFalling", true);
+            }else
+            {
+                _animator.SetBool("isFalling", false);
+            }
+
+            // if (wallSlide) return;
         }
 
         private void FixedUpdate()
         {
-            if (_jumpRequest)
+            if (_isJumping)
             {
                 _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
 
-                _jumpRequest = false;
-                _isGrounded = false;
+                _isJumping = false;
             }
 
             HandleAirTime();
         }
+
         #endregion
 
 
@@ -80,7 +115,30 @@ namespace shaman37
 
         private void Walk(Vector2 direction)
         {
-            _rb.velocity = new Vector2(direction.x * _walkingSpeed, _rb.velocity.y);
+            float deltaX = direction.x * _walkingSpeed;
+            _rb.velocity = new Vector2(deltaX, _rb.velocity.y);
+
+            _animator.SetFloat("speed", Mathf.Abs(deltaX));
+            if (!Mathf.Approximately(deltaX, 0)) {
+                // Handle animation flip
+                Vector3 scale = transform.localScale;
+                scale.x = Mathf.Sign(deltaX) * Mathf.Abs(scale.x);
+                transform.localScale = scale;
+            }
+        }
+
+        private void WallSlide()
+        {
+            wallSlide = true;
+            bool pushingWall = false;
+            if((_rb.velocity.x > 0 && _isOnRightWall) || (_rb.velocity.x < 0 && _isOnLeftWall))
+            {
+                pushingWall = true;
+            }
+
+            float push = pushingWall ? 0 : _rb.velocity.x;
+
+            _rb.velocity = new Vector2(push, -_slideSpeed);
         }
 
         private void IsGroundedCheck()
@@ -91,11 +149,28 @@ namespace shaman37
                 CapsuleDirection2D.Vertical, // Capsule's direction
                 0f, // Angle of the Capsule
                 Vector2.down, // Direction to cast the capsule [down -> ground]
-                _groundExtraHeight, // Maximum distance for the Capsule cast
+                _groundCollisionRadius, // Maximum distance for the Capsule cast
                 _mask // Detect collisions only on specified layer [default -> ground]
             );
 
             _isGrounded = rayCastHit.collider != null;
+        
+            // Debugging
+            Color rayColor = Color.red;
+            if(_isGrounded) rayColor = Color.green;
+
+            Debug.DrawRay(_collider.bounds.center, Vector2.down * (_collider.bounds.extents.y + _groundCollisionRadius), rayColor);
+        }
+
+        private void IsOnWallsCheck()
+        {
+            _isOnRightWall = Physics2D.OverlapCircle((Vector2)transform.position + _rightOffset, _wallCollisionRadius, _mask);
+            _isOnLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + _leftOffset, _wallCollisionRadius, _mask);
+
+            _isOnWall = _isOnRightWall || _isOnLeftWall;
+
+            Debug.DrawRay((Vector2)transform.position + _rightOffset, Vector2.right * _wallCollisionRadius, _isOnRightWall ? Color.green : Color.red);
+            Debug.DrawRay((Vector2)transform.position + _leftOffset, Vector2.left * _wallCollisionRadius, _isOnLeftWall ? Color.green : Color.red);
         }
         #endregion
     }
